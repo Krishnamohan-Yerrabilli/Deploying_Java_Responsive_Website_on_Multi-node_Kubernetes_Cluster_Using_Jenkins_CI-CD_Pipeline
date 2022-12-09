8148f651c8d885e05d6e37a7b276da1f4e380746
Krishnamohan Yerrabilli <krishnamohan72452964@gmail.com>
2022-12-09 10:08:01 +0530

    Create Java-installation.sh

---
+++ Server Setup/Jenkins/Java-installation.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install openjdk-8-jdk openjdk-8-jre
+set path
+cat >> /etc/environment <<EOL
+JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+JRE_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre
+EOL
