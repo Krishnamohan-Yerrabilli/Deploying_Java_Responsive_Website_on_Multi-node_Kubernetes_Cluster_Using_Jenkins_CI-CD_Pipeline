cc47f3631c1becb5ca23dff0f74f45d579b62579
Krishnamohan Yerrabilli <krishnamohan72452964@gmail.com>
2022-12-09 10:09:54 +0530

    Create Jenkins-as-a-service.sh

---
+++ Server Setup/Jenkins/Jenkins-as-a-service.sh
@@ -0,0 +1,6 @@
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+apt-get update
+apt-get install jenkins
+systemctl start jenkins
+systemctl status jenkins
